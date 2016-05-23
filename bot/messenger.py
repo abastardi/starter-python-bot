7415436d71f5e33ff573962631d2bd9066892e54
Colby Sato <cdjsato@gmail.com>
2016-05-22 18:33:31 -0700

    added first message

--- bot/messenger.py
+++ bot/messenger.py
@@ -19,9 +19,9 @@ class Messenger(object):
         channel.send_message("{}".format(msg.encode('ascii', 'ignore')))
 
     def msg_restaurant_query(self, channel_id, user_id):
-        #intro = "Hi, my name is Ollie."
+        intro = "Hi, my name is Ollie."
         # I'm here to help you find a restaurant nearby. When you give me a zip code, I'll tell you about a restaurant that's close to you. What zip code would you like to search?"
-        #self.send_message(channel_id, intro)
+        self.send_message(channel_id, intro)
         #self.clients.send_user_typing_pause(channel_id)
         #link = "http://www.yelp.com/search?find_desc=Best+Restaurants&find_loc=Duluth+MN"
         #self.send_message(channel_id, link)
